7ec1cd7c0c6b2b4be709e224698e7aa648f0b5ac
rakhmonov <uzhack@mail.ru>
2018-06-19 10:52:59 +0500

    Layout component which would render the Header and Footer

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { Router, Route } from "react-router";
+import Layout from "./components/layout";
+import "./assets/App.css";
+
+class App extends Component {
+  render() {
+      return (
+          <div>
+              <Layout>
+                  
+              </Layout>
+          </div>
+      )
+  }
+}
+
+export default App;
